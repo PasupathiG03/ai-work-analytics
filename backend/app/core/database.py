deecddc32c48a5d810d3550b5837f11ca5611de3
PasupathiG03 <pasupathi0322@gmail.com>
2026-03-31 10:44:33 +0530

    Remove retry logic for database connection and enforce SSL mode in engine creation

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -7,20 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-
-# 🔁 Retry logic
-for i in range(10):
-    try:
-        engine = create_engine(DATABASE_URL)
-        connection = engine.connect()
-        connection.close()
-        print("✅ Database connected")
-        break
-    except Exception as e:
-        print(f"⏳ DB not ready, retrying... ({i+1}/10)")
-        time.sleep(3)
-else:
-    raise Exception("❌ Could not connect to DB")
-
+engine = create_engine(DATABASE_URL, connect_args={"sslmode": "require"})
+    
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
